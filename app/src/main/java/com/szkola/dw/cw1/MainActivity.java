90fd342a4b8a6f13a3c40b7eb4924e9d95ba367c
DaV <dawid888@mail.com>
2019-02-08 01:23:45 +0100

    button zmiany jezyka

--- app/src/main/java/com/szkola/dw/cw1/MainActivity.java
+++ app/src/main/java/com/szkola/dw/cw1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.szkola.dw.cw1;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @SuppressLint("WrongViewCast")
+    public void buttonClick(View v){
+        TextView txt = findViewById(R.id.camera);
+        txt.setText("@string/aparat");
+
+        txt = findViewById(R.id.albums);
+        txt.setText("@string/album");
+
+        txt = findViewById(R.id.collage);
+        txt.setText("@string/kolaz");
+
+        txt = findViewById(R.id.network);
+        txt.setText("@string/siec");
+
+    }
 }
+
