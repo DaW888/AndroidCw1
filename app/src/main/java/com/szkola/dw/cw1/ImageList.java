c9bcc82df79def849238264554a41fa9a96b88c1
DaV <dawid888@mail.com>
2019-03-09 07:12:28 +0100

    stworzone kliki w menagerze obrazkow

--- app/src/main/java/com/szkola/dw/cw1/ImageList.java
+++ app/src/main/java/com/szkola/dw/cw1/ImageList.java
@@ -1,13 +1,14 @@
 package com.szkola.dw.cw1;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class ImageList extends AppCompatActivity {
@@ -21,11 +22,24 @@ public class ImageList extends AppCompatActivity {
         list.add("cos");
         list.add("cos");
 
+        Bundle bundle = getIntent().getExtras();
+        String folderName = bundle.getString("thisDirName");
+        Log.d("thisDirName", folderName);
+
+        File pic = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File wajda = new File(pic, "wajda");
+        File inFolder = new File(wajda, folderName);
+
+        ArrayList<File> Images = new ArrayList<>();
+        for (File dirName : inFolder.listFiles()) {
+            Images.add(dirName);
+        }
+
         lvImages = findViewById(R.id.listViewImages);
         TestAdapter adapter = new TestAdapter(
                 ImageList.this,
                 R.layout.row_with_images,
-                list
+                Images
         );
         lvImages.setAdapter(adapter);
 
--- app/src/main/java/com/szkola/dw/cw1/TestAdapter.java
+++ app/src/main/java/com/szkola/dw/cw1/TestAdapter.java
@@ -1,6 +1,9 @@
 package com.szkola.dw.cw1;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -9,19 +12,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 public class TestAdapter extends ArrayAdapter {
-    private ArrayList<String> _list;
+    private ArrayList<File> _list;
     private Context _context;
     private int _resource;
 
-    public TestAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+    public TestAdapter(@NonNull Context context, int resource, @NonNull ArrayList<File> objects) {
         super(context, resource, objects);
-        this._list = (ArrayList<String>) objects;
+        Log.d("XXX", String.valueOf(objects));
+        this._list = objects;
         this._context = context;
         this._resource = resource;
 
@@ -29,15 +32,69 @@ public class TestAdapter extends ArrayAdapter {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(_resource, null);
+        final int inPosition = position;
 
         ImageView image = convertView.findViewById(R.id.oneImage);
+        ImageView remove = convertView.findViewById(R.id.removeImage);
+        ImageView edit = convertView.findViewById(R.id.editImage);
+        ImageView info = convertView.findViewById(R.id.infoImage);
+
+
+        Uri uri = Uri.fromFile(_list.get(position));
+        image.setImageURI(uri);
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("XXX", "KLIK W OBRAZEK");
+                Log.wtf("XXX", "KLIK W OBRAZEK" + _list.get(inPosition).getPath());
+            }
+        });
+
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(_context);
+                alert.setTitle("Remove");
+                alert.setMessage(_list.get(inPosition).getPath());
+                alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alert.show();
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(_context);
+                alert.setTitle("Edit");
+                alert.setMessage(_list.get(inPosition).getPath());
+                alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alert.show();
+
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(_context);
+                alert.setTitle("Info");
+                alert.setMessage(_list.get(inPosition).getPath());
+                alert.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alert.show();
             }
         });
         return convertView;
